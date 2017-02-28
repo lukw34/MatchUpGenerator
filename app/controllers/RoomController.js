aa10ff9a12d95824dfdefef3527edb151349656c
lukw34 <lukw34@gmail.com>
2017-02-28 21:54:38 +0100

    Add emit game-ready

--- app/controllers/RoomController.js
+++ app/controllers/RoomController.js
@@ -3,14 +3,33 @@ class RoomController {
         this.io = io;
         this.roomId = `room-${roomNo}`;
         this.players = [];
-        console.log(this.roomId);
+        this.playerLimit = 2;
     }
 
     joinPlayer(player, socket) {
         socket.join(this.roomId);
-        this.io.sockets.in(this.roomId).emit('matchUp', "You are in room no. " + this.roomId);
-        const id = player.length + 1;
+        const id = (this.players.length + 1).toString();
+        this.io.sockets.in(this.roomId).emit('match-up', {id});
         this.players.push(Object.assign({}, player, {id, socket}));
+        if (id >= this.playerLimit) {
+            this._emitReady();
+        }
+    }
+
+    _emitReady() {
+        const firstPlayer = Math.round(Math.random() + this.players.length).toFixed(0),
+            game = {
+                firstPlayer,
+            };
+        this.players.forEach(({socket}, key) => {
+            const {player, id} = this.players[key],
+                me = {player, id},
+                opponents = this.players
+                    .filter(({id}) => id !== me.id)
+                    .map(({id, name}) => ({id, name})),
+                gameData = Object.assign({}, game, {me, opponents});
+            socket.emit('game-ready', gameData);
+        });
     }
 }
 
