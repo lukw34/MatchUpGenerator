d348223eff8400a52f3f782bc9661b92f08c8db4
lukw34 <lukw34@gmail.com>
2017-03-04 16:05:29 +0100

    Decorate player with positiion

--- app/controllers/LeaderboardController.js
+++ app/controllers/LeaderboardController.js
@@ -8,9 +8,9 @@ class GameController extends BaseController {
 
     save(name, points) {
         return this.Player.findOne({name}, (err, player) => {
-            if(err) {
-               logger.error(err);
-               return Promise.reject();
+            if (err) {
+                logger.error(err);
+                return Promise.reject();
             } else if (player) {
                 player.points += points;
                 return player.save()
@@ -22,14 +22,28 @@ class GameController extends BaseController {
 
     getLeaders(req, res) {
 
-        this.Player.find({}).sort('-points').limit(10).exec((err, list) => {
+        this.Player.find({}).sort('-points').limit(10).lean().exec((err, list) => {
             if (err) {
                 res.status(400).send({message: err.message})
             } else {
-                res.status(200).send(list);
+                res.status(200).send(this._addPosition(list));
             }
         });
+    }
 
+    _addPosition(players) {
+        let actualPosition = 1,
+            prevPlayerPoints;
+        return players.map(player => {
+            const {points} = player;
+             if (prevPlayerPoints > points) {
+                actualPosition += 1;
+            }
+
+            player.position = actualPosition;
+            prevPlayerPoints = points;
+            return player;
+        });
     }
 }
 module.exports = GameController;
